9ef9c3cf92a4c773add7dcc3ce6d7f88d4b6baef
luiz felipe <luizfelipe_p@hotmail.com>
2025-02-07 19:03:39 -0300

    primeiro commit reciclagem c#

---
+++ Reciclagem/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *  nas versões anteriores do dotnet 5 e anteriores a inicialização de um programa de console passou
+ * 
+ * using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+} 
+ 
+para apenas 
+
+Console.WriteLine("Hello, World!");
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+Console.WriteLine("Hello, World!");
