c8a990faea8057159c5d4b34aab8adfb52413526
luiz felipe <lf92416@gmail.com>
2025-07-29 21:04:08 -0300

    controlador basico

---
+++ Recycle-web-api/Controllers/TesteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recycle_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Teste novo controller");
+        }
+    }
+}
